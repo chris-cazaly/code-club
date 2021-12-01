60329ba0f9106fe013447c112a9f69f28b41db37
Chris Cazaly <chris.cazaly@coop.co.uk>
2021-12-01 15:24:25 +0000

    expressing the function as an object or array

--- 2021-12-01/fizzBuzz.js
+++ 2021-12-01/fizzBuzz.js
@@ -1,6 +1,7 @@
 function fizzBuzz(n){
     let numbers = [...Array(n).keys()].map(number => number + 1 )
     return numbers.reduce((result, element) => {
+        //elemitate this `if` without using `?:` nor `switch` nor `try catch`
         if((element) % 3 == 0){
             return result + "Fizz";
         }else{
@@ -11,5 +12,13 @@ function fizzBuzz(n){
 
 module.exports = fizzBuzz;
 
-// 1 + 2 + 3 = 1 + (2 + 3) = (1 + 2) + 3 = 6 = (1 + 3) + 2
-// S for all s1, s2 in S, s1 + s2 in S
+// 1 -> "1"
+// 2 -> "12"
+// 3 -> "12Fizz"
+// 4 -> "12Fizz4"
+
+fish = { 1:"1", 2:"12", 3:"12Fizz" }
+console.log(fish[3])
+
+fred = ["","1", "12","12Fizz"]
+console.log(fred[3])
